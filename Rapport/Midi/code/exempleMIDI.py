23591d8733bc9038b7353f42903505de1100354a
jcatelain <jeremy.catelain@insa-rouen.fr>
2018-01-06 22:40:57 +0100

    Modification du rapport

---
+++ Rapport/Midi/code/exempleMIDI.py
@@ -0,0 +1,14 @@
+midi.Pattern(format=1, resolution=480, tracks=\
+[midi.Track(\
+  [midi.TrackNameEvent(tick=0, text='Espana Op. 165', data=[69, 115, 112, 97, 110, 97, 32, 79, 112, 46, 32, 49, 54, 53]),
+...
+   midi.SetTempoEvent(tick=240, data=[7, 113, 125]),
+...
+   midi.EndOfTrackEvent(tick=0, data=[])]),
+ midi.Track(\
+  [midi.TrackNameEvent(tick=0, text='Piano right', data=[80, 105, 97, 110, 111, 32, 114, 105, 103, 104, 116]),
+   midi.ProgramChangeEvent(tick=0, channel=0, data=[0]),
+   midi.ControlChangeEvent(tick=0, channel=0, data=[7, 100]),
+   midi.NoteOnEvent(tick=160, channel=0, data=[64, 0]),
+   midi.NoteOnEvent(tick=160, channel=0, data=[62, 0]),
+...
