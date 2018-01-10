51e9bfddb6cf13b96f843e5bf8ed35e4f5c4fa23
jcatelain <jeremy.catelain@insa-rouen.fr>
2018-01-10 16:28:22 +0100

    Modif du rapport

--- Rapport/Midi/code/analyse.py
+++ Rapport/Midi/code/analyse.py
@@ -1,9 +1,4 @@
-import sys
-import re
-import subprocess
-import os
 import numpy as np
-import statistics as stats
 import sys
 import re
 import subprocess
@@ -13,7 +8,6 @@ import os.path
 max_ligne = 2000
 min_ligne = 440
 max_tick = 3800
-
 
 def lecture(file):
         global nbLignesSup3500,nbLignesInf3500,nbLignesSupTICK,nbLignesInfTICK
@@ -43,14 +37,19 @@ nbLignesSupTICK=0
 nbLignesInfTICK=0
 nbTickSup=0
 nbTickInf=0
-event = []
 tick = []
 data1 = []
 data2 = []
-os.chdir(os.path.dirname(os.getcwd()))
-os.chdir('Donnees')
+
+argument1 = sys.argv[1]
+os.chdir(argument1)
+
 liste=[]
+
+
+
 for root, dirs, files in os.walk(os.getcwd()):
+    dirs.clear()
     for file in files:
         if file.endswith('.txt'):
             lecture(file)
@@ -67,12 +66,10 @@ print("Maximum lignes = ",max(liste))
 print("Minimim lignes = ",min(liste))
 print("Moyenne lignes = ",int(np.mean(liste)))
 print("Mediane lignes = ",int(np.median(liste)))
-print("Mediane groupe lignes = ",stats.median_grouped(liste))
-print("Nombre de lignes > 1500 = ",nbLignesSup3500)
-print("Nombre de lignes < 1500 = ",nbLignesInf3500)
+print("Nombre de lignes > 2000 = ",nbLignesSup3500)
+print("Nombre de lignes < 2000 = ",nbLignesInf3500)
 print("Nombre de lignes  et tick > = ",nbLignesSupTICK)
 print("Nombre de lignes  et tick < = ",nbLignesInfTICK)
-#print( list(sorted(set(liste))))
 
 
 print('\n')
@@ -89,13 +86,12 @@ print("Maximum data1 = ",max(data1))
 print("Minimim data1 = ",min(data1))
 print("Moyenne data1 = ",np.mean(data1))
 print("Mediane data1 = ",np.median(data1))
+print("Ecart type data1 = ",np.std(data1))
 
 print('\n')
 print("Maximum data2 = ",max(data2))
 print("Minimim data2 = ",min(data2))
 print("Moyenne data2 = ",np.mean(data2))
 print("Mediane data2 = ",np.median(data2))
-
-#from collections import Counter
-#print(Counter(liste))
+print("Ecart type data2 = ",np.std(data2))
 
